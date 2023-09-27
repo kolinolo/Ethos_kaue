9d31b1f8431001ec4ce81c99d1f8e47db4d8b3ac
kaue <kaueh05@gmail.com>
2023-09-27 16:13:30 -0300

    acho que vai dar certo

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 function togglemode() {
 
 const html = document.documentElement
+html.classList.toggle("light")
+const img = document.querySelector("#profile img")
+if (html.classList.contains('light')){
 
-html.classList.toggle('light')
-  
+
+img.setAttribute('src','./assets/Avatar-light.png')
+
+}else{
+img.setAttribute('src','./assets/Avatar.png')}
 }
 
 
